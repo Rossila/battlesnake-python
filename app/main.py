aebf2420ddfdf890f00fe5b9557efd8fcee10795
Richard Lei <richardlei@richardlei-macbookpro2.roam.corp.google.com>
2018-03-03 13:21:49 -0800

    push

--- app/main.py
+++ app/main.py
@@ -129,29 +129,28 @@ def no_suicide(data, directions, currBoard):
     if last_dir == 'down' and 'up' in directions:
         directions.remove('up')
 
-    if 'up' in directions:
-        next_x = first.get('x')
+    if 'up' in directions: next_x = first.get('x')
         next_y = first.get('y') - 1
         print currBoard[next_x][next_y]
-        if currBoard[next_x][next_y] == nodeType.SNAKEHEAD or currBoard[next_x][next_y] == nodeType.SNAKEBODY:
+        if currBoard[next_x][next_y] == nodeType.SNAKE_HEAD or currBoard[next_x][next_y] == nodeType.SNAKE_BODY:
             directions.remove('up')
     if 'down' in directions:
         next_x = first.get('x')
         next_y = first.get('y') + 1
         print currBoard[next_x][next_y]
-        if currBoard[next_x][next_y] == nodeType.SNAKEHEAD or currBoard[next_x][next_y] == nodeType.SNAKEBODY:
+        if currBoard[next_x][next_y] == nodeType.SNAKE_HEAD or currBoard[next_x][next_y] == nodeType.SNAKE_BODY:
             directions.remove('down')
     if 'left' in directions:
         next_x = first.get('x') - 1
         next_y = first.get('y')
         print currBoard[next_x][next_y]
-        if currBoard[next_x][next_y] == nodeType.SNAKEHEAD or currBoard[next_x][next_y] == nodeType.SNAKEBODY:
+        if currBoard[next_x][next_y] == nodeType.SNAKE_HEAD or currBoard[next_x][next_y] == nodeType.SNAKE_BODY:
             directions.remove('left')
     if 'right' in directions:
         next_x = first.get('x') + 1
         next_y = first.get('y')
         print currBoard[next_x][next_y]
-        if currBoard[next_x][next_y] == nodeType.SNAKEHEAD or currBoard[next_x][next_y] == nodeType.SNAKEBODY:
+        if currBoard[next_x][next_y] == nodeType.SNAKE_HEAD or currBoard[next_x][next_y] == nodeType.SNAKE_BODY:
             directions.remove('right')
 
     print 'nosuicide dir'
